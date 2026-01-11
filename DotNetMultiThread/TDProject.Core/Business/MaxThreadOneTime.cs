1f03180c1d593353a1e44a602316022d2709b54e
tonguyenducmanh <ducmanh14032000@gmail.com>
2026-01-11 18:30:13 +0700

    đẩy tạm

---
+++ DotNetMultiThread/TDProject.Core/Business/MaxThreadOneTime.cs
@@ -0,0 +1,16 @@
+namespace TDProject.Core.Business;
+
+/// <summary>
+/// Xử lý tôi đa n thread tại 1 thời điểm, nếu số thread đã đạt tới mức tối đa
+/// Sẽ phải đợi có ít nhất 1 thread rảnh rỗi để tiếp tục làm việc
+/// </summary>
+public class MaxThreadOneTime
+{
+    #region Declare
+
+    #endregion
+
+    #region Methods
+
+    #endregion
+}
